1851481f5269a236af3e264a08caccffe146b11c
Spencer von der Ohe <s.vonderohe40@gmail.com>
2020-11-20 10:07:42 -0700

    Generate clean passages csv

--- src/preproccessing/join_passages_and_questions.py
+++ src/preproccessing/join_passages_and_questions.py
@@ -5,16 +5,18 @@
 #%%
 import pandas as pd
 
+from io import StringIO
 from pathlib import Path
 
 repo_root = Path(__file__).parent.parent.parent
 #%%
 passages_df = pd.read_csv(
     repo_root/'data/dreamscape/passages.csv',
-    encoding='utf8', # Maybe , encoding="latin-1"
+    encoding='ansi',
     quotechar="\"", 
     escapechar='\\', 
     quoting=0
 )
+passages_df = passages_df[["id", "genreId", "gradeId", "text"]].to_csv(repo_root/'data/clean/passages.csv',encoding='utf8')
 
 # %%
