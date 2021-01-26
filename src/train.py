53e5898ec09af1178db351e6c18c049a5f5d51f5
Saeed Najafi <snajafi@ualberta.ca>
2021-01-26 12:54:13 -0700

    Load pretrain

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import argparse
 from dataclasses import dataclass, field
 from typing import Optional
 
@@ -8,7 +9,7 @@ import torch.optim as optim
 from datasets import load_dataset
 from transformers import AlbertTokenizer, Trainer, TrainingArguments
 
-from src.albert_model import load_albert_encoder_decoder
+from src.albert_model import load_albert_encoder_decoder, load_pretrained_race
 
 # set the tokenizer
 tokenizer = AlbertTokenizer.from_pretrained("albert-xxlarge-v2")
@@ -115,8 +116,8 @@ def compute_metrics(pred):
     label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
 
     rouge_output = rouge.compute(
-        predictions=pred_str, references=label_str, rouge_types=["rougeL"]
-    )["rougeL"].mid
+        predictions=pred_str, references=label_str, rouge_types=["rouge2"]
+    )["rouge2"].mid
 
     return {
         "rouge2_precision": round(rouge_output.precision, 4),
@@ -176,8 +177,8 @@ def create_race_dataset():
     return train_dataset, dev_dataset, test_dataset
 
 
-def train():
-    """Main model to train."""
+def race_pretrain():
+    """main model to train."""
     train_dataset, dev_dataset, test_dataset = create_race_dataset()
 
     albert2albert = load_albert_encoder_decoder(
@@ -189,25 +190,25 @@ def train():
     albert2albert = albert2albert.to("cuda:0")
 
     # instantiate trainer
-    training_args = TrainingArguments(
+    training_args = trainingarguments(
         output_dir="./trained_models/",
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
-        do_train=True,
-        do_eval=True,
+        do_train=true,
+        do_eval=true,
         seed=10,
         gradient_accumulation_steps=1,
-        max_steps=500,
-        logging_steps=50,  # set to 1000 for full training
-        save_steps=50,  # set to 500 for full training
-        eval_steps=50,  # set to 8000 for full training
-        warmup_steps=50,  # set to 2000 for full training
-        overwrite_output_dir=True,
+        # max_steps=500,
+        logging_steps=100,  # set to 1000 for full training
+        save_steps=100,  # set to 500 for full training
+        eval_steps=100,  # set to 8000 for full training
+        warmup_steps=1000,  # set to 2000 for full training
+        overwrite_output_dir=true,
         save_total_limit=3,
-        num_train_epochs=1,
+        num_train_epochs=3,
     )
 
-    trainer = Trainer(
+    trainer = trainer(
         model=albert2albert,
         args=training_args,
         compute_metrics=compute_metrics,
@@ -217,4 +218,52 @@ def train():
     trainer.train()
 
 
-train()
+def race_predict(args):
+    """Main model to train."""
+    train_dataset, dev_dataset, test_dataset = create_race_dataset()
+
+    albert2albert = load_pretrained_race(
+        path=args.model_path,
+        mask_token_id=tokenizer.mask_token_id,
+        source_max_length=source_max_length,
+        decoder_max_length=decoder_max_length,
+    )
+
+    albert2albert = albert2albert.to("cuda:0")
+
+
+def run_main(args):
+    """Decides what to do in the code."""
+    if args.mode == "race_pretrain":
+        race_pretrain()
+    if args.mode == "race_predict":
+        race_predict(args)
+
+
+def argument_parser():
+    """augments arguments for protein-gene model."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mode",
+        type=str,
+        required=True,
+        help="race_pretrain | race_predict | main_train | main_predict",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        required=True,
+        help="Path for saving or loading models.",
+    )
+
+    parser.add_argument(
+        "--prediction_file", type=str, help="file for saving predictions"
+    )
+
+    args, _ = parser.parse_known_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = argument_parser()
+    run_main(args)
