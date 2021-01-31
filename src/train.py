c4d406462d9b9fbcbb5f9596a63e28d862c9bf5a
Saeed Najafi <snajafi@ualberta.ca>
2021-01-31 13:00:55 -0700

    dream version

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import argparse
+import io
+import json
 from dataclasses import dataclass, field
 from typing import Optional
 
@@ -7,7 +9,7 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.optim as optim
-from datasets import load_dataset
+from datasets import Dataset, load_dataset
 from transformers import AlbertTokenizer, Trainer, TrainingArguments
 
 from src.albert_model import load_albert_encoder_decoder, load_pretrained_race
@@ -16,9 +18,14 @@ from src.albert_model import load_albert_encoder_decoder, load_pretrained_race
 tokenizer = AlbertTokenizer.from_pretrained("albert-xxlarge-v2")
 tokenizer.bos_token = tokenizer.cls_token
 tokenizer.eos_token = tokenizer.sep_token
-source_max_length = 512
-decoder_max_length = 128
-batch_size = 2
+# for race
+# source_max_length = 512
+# decoder_max_length = 128
+# batch_size = 2
+# for dreamscape
+source_max_length = 256
+decoder_max_length = 64
+batch_size = 4
 
 
 def process_data_to_model_inputs(batch):
@@ -178,6 +185,132 @@ def create_race_dataset():
     return train_dataset, dev_dataset, test_dataset
 
 
+def build_dream_dataset(args):
+    data_rows = []
+    with io.open(args.dream_path, encoding="utf-8", mode="r") as fd:
+        dream_data = json.load(fd)
+        for passage_dict in dream_data:
+            text = passage_dict["text"]
+            text = " ".join(text.split())
+            for q_a_dict in passage_dict["questions_answers"]:
+                q = q_a_dict["question"]
+                a = q_a_dict["correctAnswers"][0]
+                if "__" in q:
+                    continue
+                q = " ".join(q.split())
+                a = " ".join(a.split())
+                input_str = glue_passage_question(
+                    tokenizer.bos_token, tokenizer.eos_token, text, q
+                )
+                output_str = glue_passage_question(
+                    tokenizer.bos_token, tokenizer.eos_token, a
+                )
+                data_rows.append({"inputs": input_str, "outputs": output_str})
+    df = pd.DataFrame(data_rows)
+    dataset = Dataset.from_pandas(df)
+    dataset = dataset.map(
+        process_data_to_model_inputs,
+        batched=True,
+        batch_size=batch_size,
+        remove_columns=["inputs", "outputs"],
+    )
+    dataset.set_format(
+        type="torch",
+        columns=["input_ids", "input_mask", "target_ids", "target_mask", "labels"],
+    )
+    return dataset
+
+
+def main_train(args):
+    dataset = build_dream_dataset(args)
+    albert2albert = load_pretrained_race(
+        path=args.model_path,
+        mask_token_id=tokenizer.mask_token_id,
+        source_max_length=source_max_length,
+        decoder_max_length=decoder_max_length,
+    )
+
+    albert2albert = albert2albert.to("cuda:0")
+    # instantiate trainer
+    training_args = TrainingArguments(
+        output_dir="./main_trained_models/",
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        do_train=True,
+        do_eval=False,
+        seed=10,
+        gradient_accumulation_steps=1,
+        # max_steps=500,
+        logging_steps=10,  # set to 1000 for full training
+        save_steps=10,  # set to 500 for full training
+        eval_steps=10,  # set to 8000 for full training
+        warmup_steps=10,  # set to 2000 for full training
+        overwrite_output_dir=True,
+        save_total_limit=10,
+        num_train_epochs=10,
+    )
+
+    trainer = Trainer(
+        model=albert2albert,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=dataset,
+        eval_dataset=dataset,
+    )
+    trainer.train()
+
+
+def main_predict(args):
+    dataset = build_dream_dataset(args)
+    albert2albert = load_pretrained_race(
+        path=args.model_path,
+        mask_token_id=tokenizer.mask_token_id,
+        source_max_length=source_max_length,
+        decoder_max_length=decoder_max_length,
+    )
+
+    albert2albert = albert2albert.to("cuda:0")
+
+    def generate_batch(batch):
+        input_ids = batch["input_ids"]
+        input_mask = batch["input_mask"]
+        input_ids = input_ids.to("cuda:0")
+        input_mask = input_mask.to("cuda:0")
+        predictions = albert2albert.greedy_decode(
+            input_ids=input_ids, input_mask=input_mask
+        )
+
+        # all special tokens including will be removed
+        predictions_str = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        input_str = tokenizer.batch_decode(input_ids, skip_special_tokens=False)
+
+        target_str = tokenizer.batch_decode(
+            batch["target_ids"], skip_special_tokens=True
+        )
+        output_batch = {
+            "predictions_str": predictions_str,
+            "input_str": input_str,
+            "target_str": target_str,
+        }
+        return output_batch
+
+    # instantiate trainer for prediction
+    eval_predictions = dataset.map(generate_batch, batched=True, batch_size=batch_size)
+
+    eval_predictions.set_format(
+        type="pandas", columns=["input_str", "predictions_str", "target_str"]
+    )
+    eval_predictions["predictions_str"].to_csv(
+        args.prediction_file, sep="\t", encoding="utf-8"
+    )
+    eval_predictions["input_str"].to_csv(
+        args.prediction_file + ".input.csv", sep="\t", encoding="utf-8"
+    )
+    eval_predictions["target_str"].to_csv(
+        args.prediction_file + ".target.csv", sep="\t", encoding="utf-8"
+    )
+
+
 def race_pretrain():
     """main model to train."""
     train_dataset, dev_dataset, test_dataset = create_race_dataset()
@@ -191,7 +324,7 @@ def race_pretrain():
     albert2albert = albert2albert.to("cuda:0")
 
     # instantiate trainer
-    training_args = trainingarguments(
+    training_args = TrainingArguments(
         output_dir="./trained_models/",
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
@@ -283,6 +416,10 @@ def run_main(args):
         race_pretrain()
     if args.mode == "race_predict":
         race_predict(args)
+    if args.mode == "main_train":
+        main_train(args)
+    if args.mode == "main_predict":
+        main_predict(args)
 
 
 def argument_parser():
@@ -305,6 +442,9 @@ def argument_parser():
         "--prediction_file", type=str, help="file for saving predictions"
     )
 
+    parser.add_argument(
+        "--dream_path", type=str, help="path for reading dream scape data!"
+    )
     args, _ = parser.parse_known_args()
     return args
 
