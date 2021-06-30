e8c7c7c6ca0b8ad16dd4b726d5e398c56de40944
Saeed Najafi <snajafi@ualberta.ca>
2021-06-30 15:34:22 -0600

    Save

--- train_reqa.sh
+++ train_reqa.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=reqa_train
 #SBATCH --account=def-afyshe-ab
-#SBATCH --nodes=1
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=4
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --mem=0
 #SBATCH --time=0-03:00
-#SBATCH --cpus-per-task=32
-#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
 #SBATCH --output=%N-%j.out
 
 module load python/3.8
 
 source ~/env/bin/activate
 
+export NCCL_BLOCKING_WAIT=1  #Set this environment variable if you wish to use the NCCL backend for inter-GPU communication.
 
-python src/re_qa_train.py --mode reqa_train --model_path t5_all/ --answer_checkpoint _3_model --question_checkpoint _3_model --answer_training_steps 1 --question_training_steps 3 --learning_rate 0.001 --max_epochs 4 --num_beams 16 --batch_size 4 --gpu True
+export MASTER_ADDR=$(hostname) #Store the master node’s IP address in the MASTER_ADDR environment variable.
+
+echo "r$SLURM_NODEID master: $MASTER_ADDR"
+
+
+echo "r$SLURM_NODEID Launching python script"
+
+# The SLURM_NTASKS variable tells the script how many processes are available for this execution. “srun” executes the script <tasks-per-node * nodes> times
+
+srun python src/re_qa_train.py --init_method tcp://$MASTER_ADDR:3456 --world_size $SLURM_NTASKS --mode reqa_train --model_path t5_all/ --answer_checkpoint _3_model --question_checkpoint _3_model --answer_training_steps 1 --question_training_steps 3 --learning_rate 0.001 --max_epochs 4 --num_beams 16 --batch_size 8 --gpu True
