aada6eb270961c39dd403373a0fb84e703d5eddc
SaeedNajafi <snajafi@ualberta.ca>
2021-07-08 10:01:48 -0700

    Ready to run

--- train_reqa.sh
+++ train_reqa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=reqa_train
 #SBATCH --account=def-afyshe-ab
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --tasks-per-node=4
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --mem=0
@@ -26,4 +26,4 @@ echo "All the allocated nodes: $SLURM_JOB_NODELIST"
 
 # The SLURM_NTASKS variable tells the script how many processes are available for this execution. “srun” executes the script <tasks-per-node * nodes> times
 
-srun python src/re_qa_train.py --init_method tcp://$MASTER_ADDR:3456 --world_size $SLURM_NTASKS --mode reqa_train --model_path $HOME/reqa_models/ --answer_checkpoint _3_model --question_checkpoint _3_model --answer_training_steps 1 --question_training_steps 3 --learning_rate 0.001 --max_epochs 4 --num_beams 4 --batch_size 8 --gpu True
+srun python src/re_qa_train.py --init_method tcp://$MASTER_ADDR:3456 --world_size $SLURM_NTASKS --mode reqa_train --model_path $HOME/reqa_models/ --answer_checkpoint _3_model --question_checkpoint _3_model --answer_training_steps 1 --question_training_steps 3 --learning_rate 0.001 --max_epochs 4 --num_beams 32 --batch_size 16  --gpu True --num_workers 6
