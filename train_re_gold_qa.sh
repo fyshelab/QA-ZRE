437d1ec0ed55c9aa5bf82f6ef7cdb4f3dff6dacb
SaeedNajafi <snajafi@ualberta.ca>
2021-07-21 13:40:47 -0700

    exp1

--- train_re_gold_qa.sh
+++ train_re_gold_qa.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+#SBATCH --job-name=re_gold_qa_train
+#SBATCH --account=rrg-afyshe
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=32000M
+#SBATCH --time=0-15:00
+#SBATCH --cpus-per-task=6
+#SBATCH --output=%N-%j.out
+
+module load python/3.8
+module load StdEnv/2020  gcc/9.3.0 arrow/2.0.0
+
 source env/bin/activate
 
-python src/re_gold_qa_train.py --mode re_gold_qa_train --model_path $HOME/re_gold_qa_models/ --checkpoint _3_model --learning_rate 0.001 --max_epochs 4 --batch_size 64  --gpu True --train zero-shot-extraction/relation_splits/train.0 --dev zero-shot-extraction/relation_splits/dev.0 
+export NCCL_BLOCKING_WAIT=1  #Set this environment variable if you wish to use the NCCL backend for inter-GPU communication.
+
+export MASTER_ADDR=$(hostname) #Store the master node’s IP address in the MASTER_ADDR environment variable.
+
+echo "r$SLURM_NODEID master: $MASTER_ADDR"
+
+echo "r$SLURM_NODEID Launching python script"
+
+echo "All the allocated nodes: $SLURM_JOB_NODELIST"
+
+# The SLURM_NTASKS variable tells the script how many processes are available for this execution. “srun” executes the script <tasks-per-node * nodes> times
+
+srun python src/re_gold_qa_train.py --mode re_gold_qa_train --init_method tcp://$MASTER_ADDR:3456 --world_size $SLURM_NTASKS --model_path $HOME/re_gold_qa_models/ --checkpoint _3_model --learning_rate 0.001 --max_epochs 2 --batch_size 3  --gpu True --train zero-shot-extraction/relation_splits/train.very_small.0 --dev zero-shot-extraction/relation_splits/dev.0
