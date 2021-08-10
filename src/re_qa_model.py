46bdff8d40da385084196de69ac9272d6c32bc2d
Saeed Najafi <snajafi@ualberta.ca>
2021-08-10 15:21:12 -0600

    Final run of MML-PGG-BS on RE dataset

--- src/re_qa_model.py
+++ src/re_qa_model.py
@@ -12,6 +12,8 @@ import numpy
 import torch
 from transformers import Adafactor, T5ForConditionalGeneration, T5Tokenizer
 
+from src.nq_utils import white_space_fix
+
 
 @dataclass
 class HyperParameters:
@@ -125,6 +127,7 @@ def prob_of_sampled_predictions(loss_fct, sample_outputs):
 
 MODEL_NAME = "t5-base"
 # MODEL_NAME = "allenai/unifiedqa-t5-base"
+# Q_MODEL_NAME = "mrm8488/t5-base-finetuned-question-generation-ap"
 Q_MODEL_NAME = "iarfmoose/t5-base-question-generator"
 
 
@@ -182,9 +185,10 @@ class REQA(torch.nn.Module):
                 os.makedirs(cfg.model_path)
             self.model_path = os.path.join(cfg.model_path, "model")
 
-            if load_answer:
-                # we have a pre-trained answer module.
-                load_module(answer_model, self.model_path, cfg.answer_checkpoint)
+            # if load_answer:
+            # we have a pre-trained answer module.
+            load_module(answer_model, self.model_path, cfg.answer_checkpoint)
+            load_module(question_model, self.model_path, cfg.question_checkpoint)
 
         elif cfg.mode in ["test", "inference"]:
             self.model_path = os.path.join(cfg.model_path, "model")
@@ -205,15 +209,21 @@ class REQA(torch.nn.Module):
             question_input_ids = question_input_ids.to(current_device)
             question_input_mask = question_input_mask.to(current_device)
 
-        question_predictions = self.question_model.generate(
-            input_ids=question_input_ids,
-            attention_mask=question_input_mask,
-        )
+        with torch.no_grad():
+            question_predictions = self.question_model.generate(
+                input_ids=question_input_ids,
+                attention_mask=question_input_mask,
+            )
 
         question_predictions_str = self.question_tokenizer.batch_decode(
             question_predictions, skip_special_tokens=True
         )
 
+        question_predictions_str = [
+            remove_prefix(pred, "question: ") for pred in question_predictions_str
+        ]
+        # print(question_predictions_str)
+        # print(batch["contexts"])
         new_articles = []
         for i in range(len(batch["passages"])):
             new_article = (
@@ -241,14 +251,16 @@ class REQA(torch.nn.Module):
 
         return answer_input_ids, answer_input_mask
 
-    def predict_step(self, batch):
+    def predict_step(self, batch, current_device):
         # Free memory in GPU, very important!
         clear_cache()
         # disable dropout
         self.answer_model.eval()
         self.question_model.eval()
 
-        answer_input_ids, answer_input_mask = self.question_greedy_predict(batch)
+        answer_input_ids, answer_input_mask = self.question_greedy_predict(
+            batch, current_device
+        )
 
         second_entity_predictions = self.answer_model.generate(
             input_ids=answer_input_ids,
@@ -257,6 +269,7 @@ class REQA(torch.nn.Module):
         second_entity_predictions_str = self.answer_tokenizer.batch_decode(
             second_entity_predictions, skip_special_tokens=True
         )
+        # print(second_entity_predictions_str)
         for index in range(len(second_entity_predictions_str)):
             pred_str = second_entity_predictions_str[index]
             output_batch = {
@@ -265,16 +278,9 @@ class REQA(torch.nn.Module):
             yield output_batch
 
     def pgg_answer_training(self, batch, current_device):
-        self.question_model.eval()
-        self.question_optimizer.zero_grad()
-
         answer_input_ids, answer_input_mask = self.question_greedy_predict(
             batch, current_device
         )
-
-        self.answer_model.train()
-        self.answer_optimizer.zero_grad()
-
         target_mask = batch["second_entity_attention_mask"]
         labels = batch["second_entity_labels"]
         if self.config.gpu:
@@ -292,156 +298,9 @@ class REQA(torch.nn.Module):
         loss = re_loss
         loss_value = loss.item()
 
-        if math.isnan(loss):
-            return {"loss_value": loss_value}
-
-        # BackProp
-        loss.backward()
-
-        # Optimize
-        self.answer_optimizer.step()
-
-        return {"loss_value": loss_value}
-
-    def pg_question_training(self, batch, current_device):
-        self.question_optimizer.zero_grad()
-        self.answer_optimizer.zero_grad()
-        self.question_model.train()
-        self.answer_model.eval()
-
-        loss_fct = torch.nn.CrossEntropyLoss(ignore_index=-100, reduction="none")
-        if self.config.gpu:
-            loss_fct = loss_fct.to(current_device)
-
-        # Loss from the entity relation examples!
-        question_input_ids = batch["entity_relation_passage_input_ids"]
-        question_input_mask = batch["entity_relation_passage_attention_mask"]
-        if self.config.gpu:
-            question_input_ids = question_input_ids.to(current_device)
-            question_input_mask = question_input_mask.to(current_device)
-
-        b_sz, _ = question_input_ids.size()
-
-        # Use diverse beam search to collect samples.
-        beam_question_outputs = self.question_model.generate(
-            input_ids=question_input_ids,
-            attention_mask=question_input_mask,
-            no_repeat_ngram_size=self.config.no_repeat_ngram_size,
-            early_stopping=self.config.early_stopping,
-            max_length=self.config.decoder_max_length,
-            num_return_sequences=self.config.num_beams,
-            num_beams=self.config.num_beams,
-            num_beam_groups=self.config.num_beam_groups,
-            diversity_penalty=self.config.beam_diversity_penalty,
-            output_scores=True,
-            return_dict_in_generate=True,
-        )
-
-        beam_questions, beam_log_p = prob_of_sampled_predictions(
-            loss_fct, beam_question_outputs
-        )
-
-        re_log_p_question = beam_log_p.view(self.config.num_beams, b_sz)
-
-        beam_question_predictions_str = self.question_tokenizer.batch_decode(
-            beam_questions, skip_special_tokens=True
-        )
-
-        beam_question_predictions_str_reshaped = [
-            beam_question_predictions_str[
-                i * (self.config.num_beams) : (i + 1) * (self.config.num_beams)
-            ]
-            for i in range(b_sz)
-        ]
-
-        new_articles = []
-        for i in range(b_sz):
-            for j in range(self.config.num_beams):
-                new_article = (
-                    "question: "
-                    + beam_question_predictions_str_reshaped[i][j]
-                    + " context: "
-                    + batch["passages"][i]
-                    + " </s>"
-                )
-                new_articles.append(new_article)
-
-        answer_inputs = self.answer_tokenizer(
-            new_articles,
-            truncation=True,
-            padding="max_length",
-            max_length=self.config.source_max_length,
-            add_special_tokens=False,
-            return_tensors="pt",
-        )
-
-        answer_input_ids = answer_inputs.input_ids
-        answer_input_mask = answer_inputs.attention_mask
-        if self.config.gpu:
-            answer_input_ids = answer_input_ids.to(current_device)
-            answer_input_mask = answer_input_mask.to(current_device)
-
-        target_mask = batch["second_entity_attention_mask"]
-        labels = batch["second_entity_labels"]
-        if self.config.gpu:
-            target_mask = target_mask.to(current_device)
-            labels = labels.to(current_device)
-
-        b_sz, seq_len = labels.size()
-        labels = labels.repeat(1, self.config.num_beams).view(-1, seq_len)
-        target_mask = target_mask.repeat(1, self.config.num_beams).view(-1, seq_len)
-
-        output = self.answer_model(
-            input_ids=answer_input_ids,
-            attention_mask=answer_input_mask,
-            decoder_attention_mask=target_mask,
-            decoder_input_ids=self.answer_model._shift_right(labels),
-            labels=None,
-        )
-
-        log_p = -loss_fct(
-            output.logits.view(-1, output.logits.size(-1)),
-            labels.view(-1),
-        )
-
-        b, sz, v = output.logits.size()
-        log_p = log_p.view(b, sz)
-        good_log_p = log_p.masked_fill_(labels == -100, 0.0)
-        answer_log_p = torch.sum(good_log_p, dim=1).squeeze()
-        answer_log_p = answer_log_p.view(self.config.num_beams, b_sz)
-
-        # Psuedo loss for the policy gradient.
-        re_loss = -torch.mean(
-            torch.mean(
-                torch.mul(
-                    torch.transpose(answer_log_p, 0, 1),
-                    torch.transpose(re_log_p_question, 0, 1),
-                ),
-                dim=1,
-            ),
-            dim=0,
-        )
-
-        loss = re_loss
-        loss_value = loss.item()
-
-        if math.isnan(loss):
-            return {"loss_value": loss_value}
-
-        # BackProp
-        loss.backward()
-
-        # Optimize
-        self.question_optimizer.step()
-
-        return {"loss_value": loss_value}
+        return loss, loss_value
 
     def mml_question_training(self, batch, current_device):
-        self.question_optimizer.zero_grad()
-        self.answer_optimizer.zero_grad()
-        self.question_model.train()
-        self.answer_model.eval()
-
         loss_fct = torch.nn.CrossEntropyLoss(ignore_index=-100, reduction="none")
         if self.config.gpu:
             loss_fct = loss_fct.to(current_device)
@@ -455,31 +314,36 @@ class REQA(torch.nn.Module):
 
         b_sz, _ = question_input_ids.size()
 
-        # Use diverse beam search to collect samples.
-        beam_question_outputs = self.question_model.generate(
-            input_ids=question_input_ids,
-            attention_mask=question_input_mask,
-            no_repeat_ngram_size=self.config.no_repeat_ngram_size,
-            early_stopping=self.config.early_stopping,
-            max_length=self.config.decoder_max_length,
-            num_return_sequences=self.config.num_beams,
-            num_beams=self.config.num_beams,
-            num_beam_groups=self.config.num_beam_groups,
-            diversity_penalty=self.config.beam_diversity_penalty,
-            output_scores=True,
-            return_dict_in_generate=True,
-        )
+        with torch.no_grad():
+            # Use diverse beam search to collect samples.
+            beam_question_outputs = self.question_model.generate(
+                input_ids=question_input_ids,
+                attention_mask=question_input_mask,
+                no_repeat_ngram_size=self.config.no_repeat_ngram_size,
+                early_stopping=self.config.early_stopping,
+                max_length=self.config.decoder_max_length,
+                num_return_sequences=self.config.num_beams,
+                num_beams=self.config.num_beams,
+                # num_beam_groups=self.config.num_beam_groups,
+                # diversity_penalty=self.config.beam_diversity_penalty,
+                output_scores=True,
+                return_dict_in_generate=True,
+            )
 
-        beam_questions, beam_log_p = prob_of_sampled_predictions(
-            loss_fct, beam_question_outputs
-        )
-
-        re_log_p_question = beam_log_p.view(self.config.num_beams, b_sz)
+            beam_questions, _ = prob_of_sampled_predictions(
+                loss_fct, beam_question_outputs
+            )
+            # re_log_p_question = beam_log_p.view(self.config.num_beams, b_sz)
 
         beam_question_predictions_str = self.question_tokenizer.batch_decode(
             beam_questions, skip_special_tokens=True
         )
 
+        beam_question_predictions_str = [
+            remove_prefix(pred, "question: ") for pred in beam_question_predictions_str
+        ]
+        # print(batch["contexts"])
+        # print(beam_question_predictions_str)
         beam_question_predictions_str_reshaped = [
             beam_question_predictions_str[
                 i * (self.config.num_beams) : (i + 1) * (self.config.num_beams)
@@ -524,13 +388,14 @@ class REQA(torch.nn.Module):
         labels = labels.repeat(1, self.config.num_beams).view(-1, seq_len)
         target_mask = target_mask.repeat(1, self.config.num_beams).view(-1, seq_len)
 
-        output = self.answer_model(
-            input_ids=answer_input_ids,
-            attention_mask=answer_input_mask,
-            decoder_attention_mask=target_mask,
-            decoder_input_ids=self.answer_model._shift_right(labels),
-            labels=None,
-        )
+        with torch.no_grad():
+            output = self.answer_model(
+                input_ids=answer_input_ids,
+                attention_mask=answer_input_mask,
+                decoder_attention_mask=target_mask,
+                decoder_input_ids=self.answer_model._shift_right(labels),
+                labels=None,
+            )
 
         log_p = -loss_fct(
             output.logits.view(-1, output.logits.size(-1)),
@@ -543,13 +408,78 @@ class REQA(torch.nn.Module):
         answer_log_p = torch.sum(good_log_p, dim=1).squeeze()
         answer_log_p = answer_log_p.view(self.config.num_beams, b_sz)
 
-        # Psuedo loss for the policy gradient.
+        # Now re-run the question generator and compute the loss for the beam predictions.
+        # This will compute the gradients in the question module.
+        b_sz, seq_len = question_input_ids.size()
+        question_input_ids = question_input_ids.repeat(1, self.config.num_beams).view(
+            -1, seq_len
+        )
+        question_input_mask = question_input_mask.repeat(1, self.config.num_beams).view(
+            -1, seq_len
+        )
+        output_questions = []
+        for i in range(b_sz):
+            for j in range(self.config.num_beams):
+                output_question = (
+                    white_space_fix(beam_question_predictions_str_reshaped[i][j])
+                    + " </s>"
+                )
+                output_questions.append(output_question)
+
+        question_encodings = self.question_tokenizer(
+            output_questions,
+            truncation=True,
+            padding="max_length",
+            max_length=self.config.decoder_max_length,
+            add_special_tokens=False,
+            return_tensors="pt",
+        )
+        question_labels = question_encodings.pop("input_ids")
+        question_target_mask = question_encodings.pop("attention_mask")
+
+        # because HuggingFace automatically shifts the labels, the labels correspond exactly to `target_ids`.
+        # We have to make sure that the PAD token is ignored
+
+        question_labels = [
+            [
+                -100 if token == self.question_tokenizer.pad_token_id else token
+                for token in labels
+            ]
+            for labels in question_labels.tolist()
+        ]
+        question_labels = torch.tensor(question_labels)
+        if self.config.gpu:
+            question_target_mask = question_target_mask.to(current_device)
+            question_labels = question_labels.to(current_device)
+
+        clear_cache()
+
+        question_output = self.question_model(
+            input_ids=question_input_ids,
+            attention_mask=question_input_mask,
+            decoder_attention_mask=question_target_mask,
+            decoder_input_ids=self.question_model._shift_right(question_labels),
+            labels=None,
+        )
+
+        log_question_p = -loss_fct(
+            question_output.logits.view(-1, question_output.logits.size(-1)),
+            question_labels.view(-1),
+        )
+
+        b, sz, v = question_output.logits.size()
+        log_question_p = log_question_p.view(b, sz)
+        good_log_question_p = log_question_p.masked_fill_(question_labels == -100, 0.0)
+        question_log_p = torch.sum(good_log_question_p, dim=1).squeeze()
+        question_log_p = question_log_p.view(self.config.num_beams, b_sz)
+
+        # MML
         re_loss = -torch.mean(
             torch.log(
                 torch.sum(
                     torch.mul(
-                        torch.transpose(torch.exp(answer_log_p.detach()), 0, 1),
-                        torch.transpose(torch.exp(re_log_p_question), 0, 1),
+                        torch.transpose(torch.exp(answer_log_p), 0, 1),
+                        torch.transpose(torch.exp(question_log_p), 0, 1),
                     ),
                     dim=1,
                 )
@@ -560,16 +490,7 @@ class REQA(torch.nn.Module):
         loss = re_loss
         loss_value = loss.item()
 
-        if math.isnan(loss):
-            return {"loss_value": loss_value}
-
-        # BackProp
-        loss.backward()
-
-        # Optimize
-        self.question_optimizer.step()
-
-        return {"loss_value": loss_value}
+        return loss, loss_value
 
     def train_step(
         self,
@@ -586,3 +507,35 @@ class REQA(torch.nn.Module):
         elif phase == "question":
             # return self.pg_question_training(batch, current_device)
             return self.mml_question_training(batch, current_device)
+
+    def sim_train(self, batch, current_device):
+        clear_cache()
+        self.question_optimizer.zero_grad()
+        self.answer_optimizer.zero_grad()
+        self.question_model.train()
+        self.answer_model.train()
+
+        answer_loss, answer_loss_value = self.pgg_answer_training(batch, current_device)
+        clear_cache()
+        question_loss, question_loss_value = self.mml_question_training(
+            batch, current_device
+        )
+
+        output = {}
+        if not math.isnan(answer_loss_value):
+            # BackProp
+            answer_loss.backward()
+            # Optimize
+            self.answer_optimizer.step()
+
+        output["answer_loss_value"] = answer_loss_value
+
+        if not math.isnan(question_loss_value):
+            # BackProp
+            question_loss.backward()
+            # Optimize
+            self.question_optimizer.step()
+
+        output["question_loss_value"] = question_loss_value
+
+        return output
--- src/re_qa_train.py
+++ src/re_qa_train.py
@@ -501,6 +501,19 @@ def run_train_epoch(
             yield step, loss_values["loss_value"]
 
 
+def sim_run_train_epoch(model, train_dataloader, current_device) -> Generator:
+    """Train the model and return the loss for 'num_steps' given the
+    'batch_size' and the train_dataset.
+
+    Randomly pick a batch from the train_dataset.
+    """
+    step = 0
+    for main_batch in train_dataloader:
+        loss_values = model.sim_train(main_batch, current_device)
+        step += 1
+        yield step, loss_values["answer_loss_value"], loss_values["question_loss_value"]
+
+
 def run_predict(model, dev_dataloader, prediction_file: str) -> None:
     """Read the 'dev_dataset' and predict results with the model, and save the
     results in the prediction_file."""
@@ -530,6 +543,113 @@ def save_config(config: HyperParameters, path: str) -> None:
         os.path.join(path, "config.ini"), mode="w", encoding="utf-8"
     ) as configfile:
         parser.write(configfile)
+
+
+def sim_run_model(
+    model,
+    config,
+    train_dataloader=None,
+    dev_dataloader=None,
+    test_dataloader=None,
+    save_always: Optional[bool] = False,
+    rank=0,
+    train_samplers=None,
+    current_device=0,
+) -> None:
+    """Run the model on input data (for training or testing)"""
+
+    model_path = config.model_path
+    max_epochs = config.max_epochs
+    mode = config.mode
+    if mode == "train":
+        print("\nRank: {0} | INFO: ML training\n".format(rank))
+        first_start = time.time()
+        epoch = 0
+        while epoch < max_epochs:
+            # let all processes sync up before starting with a new epoch of training
+            dist.barrier()
+
+            # make sure we get different orderings.
+            for sampler in train_samplers:
+                sampler.set_epoch(epoch)
+
+            print("\nRank: {0} | Epoch:{1}\n".format(rank, epoch))
+            start = time.time()
+
+            question_total_loss = []
+            answer_total_loss = []
+            for step, answer_loss, question_loss in sim_run_train_epoch(
+                model, train_dataloader, current_device
+            ):
+                if question_loss:
+                    question_total_loss.append(question_loss)
+
+                if answer_loss:
+                    answer_total_loss.append(answer_loss)
+
+                if question_total_loss:
+                    question_mean_loss = np.mean(question_total_loss)
+
+                if answer_total_loss:
+                    answer_mean_loss = np.mean(answer_total_loss)
+
+                print(
+                    "\rRank:{0} | Batch:{1} | Question Loss:{2} | Question Mean Loss:{3} | Answer Loss:{4} | Answer Mean Loss:{5} | GPU Usage:{6}\n".format(
+                        rank,
+                        step,
+                        question_loss,
+                        question_mean_loss,
+                        answer_loss,
+                        answer_mean_loss,
+                        torch.cuda.memory_allocated(device=current_device),
+                    )
+                )
+                if rank == 0 and save_always and step > 0 and (step % 1000 == 0):
+                    save(
+                        model.question_model,
+                        model.model_path,
+                        str(epoch) + "_question_step_" + str(step),
+                    )
+                    save(
+                        model.answer_model,
+                        model.model_path,
+                        str(epoch) + "_answer_step_" + str(step),
+                    )
+
+                if save_always and step > 0 and (step % 1000 == 0):
+                    dist.barrier()
+
+            if rank == 0 and save_always:
+                save(
+                    model.question_model,
+                    model.model_path,
+                    str(epoch) + "_question_full",
+                )
+                save(
+                    model.answer_model,
+                    model.model_path,
+                    str(epoch) + "_answer_full",
+                )
+
+            msg = "\nRank: {0} | Epoch training time: {1} seconds\n".format(
+                rank, time.time() - start
+            )
+            print(msg)
+            epoch += 1
+
+        if rank == 0:
+            save_config(config, model_path)
+        msg = "\nRank: {0} | Total training time: {1} seconds\n".format(
+            rank, time.time() - first_start
+        )
+        print(msg)
+
+    elif mode == "test":
+        print("Predicting...")
+        start = time.time()
+        run_predict(model, test_dataloader, config.prediction_file)
+        msg = "\nTotal prediction time:{} seconds\n".format(time.time() - start)
+        print(msg)
 
 
 def run_model(
