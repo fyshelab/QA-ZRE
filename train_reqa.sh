99e8f8a613269a9ffaa671da9cd2414d39af4929
SaeedNajafi <snajafi@ualberta.ca>
2021-08-15 10:40:45 -0700

    Save

--- train_reqa.sh
+++ train_reqa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=reqa_mml_pgg_bs_train
 #SBATCH --account=rrg-afyshe
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --tasks-per-node=4
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --mem=0
