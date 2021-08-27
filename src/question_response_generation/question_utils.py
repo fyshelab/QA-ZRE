96fa28ae2bd82989ee4ea08156d542348df62fe6
Saeed Najafi <snajafi@ualberta.ca>
2021-08-27 12:34:15 -0600

    Save code for question generation

--- src/question_response_generation/question_utils.py
+++ src/question_response_generation/question_utils.py
@@ -1,12 +1,47 @@
 import random
 
+import spacy
 import torch
 from datasets import load_dataset
+from spacy.lang.en.stop_words import STOP_WORDS
 from torch.utils.data import DataLoader
 
 
 def white_space_fix(text):
     return " ".join(text.split())
+
+
+def q_only_read_narrative_dataset():
+    """Read the narrative qa dataset for question generation"""
+
+    def process_narrative_row(row):
+        """Helper functions for NarrativeQA Dataset."""
+        question = row["question"]["text"]
+        article = row["document"]["summary"]["text"]
+        return {
+            "article": white_space_fix(article),
+            "question": white_space_fix(question),
+        }
+
+    train_dataset = load_dataset("narrativeqa", split="train")
+    dev_dataset = load_dataset("narrativeqa", split="validation")
+    test_dataset = load_dataset("narrativeqa", split="test")
+
+    train_dataset = train_dataset.map(
+        process_narrative_row,
+        remove_columns=["document", "answers"],
+    )
+
+    dev_dataset = dev_dataset.map(
+        process_narrative_row,
+        remove_columns=["document", "answers"],
+    )
+
+    test_dataset = test_dataset.map(
+        process_narrative_row,
+        remove_columns=["document", "answers"],
+    )
+    return train_dataset, dev_dataset, test_dataset
 
 
 def q_read_narrative_dataset():
@@ -48,6 +83,28 @@ def q_read_narrative_dataset():
     return train_dataset, dev_dataset, test_dataset
 
 
+def q_only_read_squad_dataset():
+    def process_squad_row(row):
+        context = row["context"]
+        question = row["question"]
+        return {
+            "article": white_space_fix(context),
+            "question": white_space_fix(question),
+        }
+
+    train_dataset = load_dataset("squad_v2", split="train")
+    train_dataset = train_dataset.map(
+        process_squad_row,
+        remove_columns=["id", "title", "context", "answers"],
+    ).filter(lambda row: row["article"] != "NONE")
+    dev_dataset = load_dataset("squad_v2", split="validation")
+    dev_dataset = dev_dataset.map(
+        process_squad_row,
+        remove_columns=["id", "title", "context", "answers"],
+    ).filter(lambda row: row["article"] != "NONE")
+    return train_dataset, dev_dataset, dev_dataset
+
+
 def q_read_squad_dataset():
     def process_squad_row(row):
         context = row["context"]
@@ -75,6 +132,34 @@ def q_read_squad_dataset():
     dev_dataset = dev_dataset.map(
         process_squad_row,
         remove_columns=["id", "title", "context", "question", "answers"],
+    ).filter(lambda row: row["article"] != "NONE")
+    return train_dataset, dev_dataset, dev_dataset
+
+
+def q_only_read_drop_dataset():
+    def process_drop_row(row):
+        context = row["passage"]
+        question = row["question"]
+        return {
+            "article": white_space_fix(context),
+            "question": white_space_fix(question),
+        }
+
+    train_dataset = load_dataset("drop", split="train")
+    train_dataset = train_dataset.map(
+        process_drop_row,
+        remove_columns=[
+            "passage",
+            "answers_spans",
+        ],
+    ).filter(lambda row: row["article"] != "NONE")
+    dev_dataset = load_dataset("drop", split="validation")
+    dev_dataset = dev_dataset.map(
+        process_drop_row,
+        remove_columns=[
+            "passage",
+            "answers_spans",
+        ],
     ).filter(lambda row: row["article"] != "NONE")
     return train_dataset, dev_dataset, dev_dataset
 
@@ -291,3 +376,103 @@ def create_question_dataset(
         test_dataset,
         train_sampler,
     )
+
+
+def create_data_for_question_generation():
+    nq_train_dataset, _, _ = q_only_read_narrative_dataset()
+    squad_train_dataset, _, _ = q_only_read_squad_dataset()
+    drop_train_dataset, _, _ = q_only_read_drop_dataset()
+
+    train_contexts = []
+    train_questions = []
+    for row in nq_train_dataset:
+        train_contexts.append(row["article"])
+        train_questions.append(row["question"])
+
+    for row in squad_train_dataset:
+        train_contexts.append(row["article"])
+        train_questions.append(row["question"])
+
+    for row in drop_train_dataset:
+        train_contexts.append(row["article"])
+        train_questions.append(row["question"])
+
+    nlp = spacy.load("en_core_web_sm")
+    # Add these interrogative words into the stop lists.
+    stop_list = [
+        "what",
+        "for",
+        "when",
+        "what for",
+        "where",
+        "which",
+        "who",
+        "whom",
+        "whose",
+        "why",
+        "why don't",
+        "how",
+        "how far",
+        "how long",
+        "how much",
+        "how many",
+        "how old",
+        "how come",
+    ]
+
+    STOP_WORDS.update(stop_list)
+
+    for word in STOP_WORDS:
+        lexeme = nlp.vocab[word]
+        lexeme.is_stop = True
+
+    good_qs = []
+    for index, q in enumerate(train_questions):
+        q_doc = nlp(q)
+        new_q_doc = [token.lemma_ for token in q_doc if token.lemma_ != "-PRON-"]
+        new_q = u" ".join(new_q_doc)
+        doc_to_remove_stop = nlp.make_doc(new_q)
+        final_doc = [
+            token.text
+            for token in doc_to_remove_stop
+            if token.is_stop != True and token.is_punct != True
+        ]
+        q_ents = [(e.text, e.label_) for e in q_doc.ents]
+        # to collect high precision data.
+        if len(q_ents) == 1:
+            new_final_doc = []
+            for token in final_doc:
+                if token not in q_ents[0][0]:
+                    new_final_doc.append(token)
+            if new_final_doc:
+                good_qs.append(
+                    (train_contexts[index], q, " ".join(new_final_doc), q_ents[0][0])
+                )
+
+    contexts = []
+    questions = []
+    for row in good_qs:
+        context = row[0]
+        question = row[1]
+        tokens = row[2].split(" ")
+        if len(tokens) > 0 and len(tokens) < 6:
+            relation_signal = " ".join(tokens)
+
+        if len(tokens) >= 6:
+            token_num = random.randint(3, len(tokens))
+            sampled_tokens = random.sample(tokens, token_num)
+            relation_signal = " ".join(sampled_tokens)
+        else:
+            continue
+        contexts.append(
+            "answer: "
+            + white_space_fix(row[3])
+            + " <SEP> "
+            + white_space_fix(relation_signal)
+            + " context: "
+            + white_space_fix(context)
+            + " </s>"
+        )
+        questions.append(white_space_fix(question) + " </s>")
+
+    return contexts, questions
