b557db8eb842809b7e139ac9a5349d96a3e364c0
Saeed Najafi <snajafi@ualberta.ca>
2021-09-27 13:02:22 -0600

    Fixed the batch , sample issue

--- src/re_qa_test.py
+++ src/re_qa_test.py
@@ -1,6 +1,65 @@
 import torch
 
-from src.re_qa_model import REQA, HyperParameters, load_module
+from src.re_qa_model import (REQA, HyperParameters, load_module,
+                             prepare_response_module_input)
+
+
+def test_prepare_response_module_input():
+    """Test that if the input processing for the response module is correct."""
+
+    # -100 is the pad token.
+    labels = torch.tensor([[0, 1, 2, 3, -100, -100, -100], [0, 5, 6, 7, 8, -100, -100]])
+    target_mask = torch.tensor([[1, 1, 1, 1, 0, 0, 0], [1, 1, 1, 1, 1, 0, 0]])
+    sample_mask = torch.tensor([[1, 0], [1, 1]])
+    input_ids = torch.tensor(
+        [[0, 1, 2, 4, 4], [0, 1, 3, 4, 4], [0, 5, 6, 7, 4], [0, 5, 6, 6, 4]]
+    )
+    input_mask = torch.tensor(
+        [[1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 1, 0], [1, 1, 1, 1, 0]]
+    )
+    (
+        answer_input_ids,
+        answer_input_mask,
+        target_mask,
+        new_labels,
+    ) = prepare_response_module_input(
+        labels=labels,
+        num_samples=2,
+        target_mask=target_mask,
+        sample_masks=sample_mask,
+        answer_input_ids=input_ids,
+        answer_input_mask=input_mask,
+    )
+
+    assert answer_input_ids.tolist() == [
+        [0, 1, 2, 4, 4],
+        [0, 0, 0, 0, 0],
+        [0, 5, 6, 7, 4],
+        [0, 5, 6, 6, 4],
+    ]
+
+    assert answer_input_mask.tolist() == [
+        [1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 0],
+    ]
+
+    assert target_mask.tolist() == [
+        [1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 1, 0, 0],
+    ]
+
+    # the second sample in the first row of the batch is dummy sample.
+    assert new_labels.tolist() == [
+        [0, 1, 2, 3, -100, -100, -100],
+        [-100, -100, -100, -100, -100, -100, -100],
+        [0, 5, 6, 7, 8, -100, -100],
+        [0, 5, 6, 7, 8, -100, -100],
+    ]
+    assert new_labels.size() == (4, 7)
 
 
 def test_model_prediction():
