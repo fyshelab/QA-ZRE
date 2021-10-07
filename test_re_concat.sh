9a3bcb3f32aaaec6ce856f27f4d189ddc118a632
SaeedNajafi <snajafi@ualberta.ca>
2021-10-07 07:34:50 +0000

    save

--- test_re_concat.sh
+++ test_re_concat.sh
@@ -7,11 +7,11 @@ for (( i=1; i<=131; i++ ))
 do
 	step=$((i * 100))
 	printf "step ${step}\r\n"
-	gsutil -m cp gs://acl-2022-storage/concat_fold_1/model_0_step_${step}_model $HOME/concat_fold_1/
+	gsutil -m cp gs://acl-2022-storage/concat_fold_1/model_1_step_${step}_model $HOME/concat_fold_1/
 	python src/re_gold_qa_train.py \
 		--mode re_concat_qa_test \
 		--model_path $HOME/concat_fold_1/ \
-		--checkpoint _0_step_${step}_model \
+		--checkpoint _1_step_${step}_model \
 		--learning_rate 0.001 --max_epochs 1 \
 		--concat_questions False \
 		--batch_size 64  --gpu True \
@@ -20,17 +20,17 @@ do
 		--dev zero-shot-extraction/relation_splits/dev.0 \
 		--gpu_device 0 \
 		--seed 12321 \
-		--prediction_file $HOME/concat_fold_1/concat_fold_1.dev.predictions.0.step.${step}.csv
-	gsutil -m cp $HOME/concat_fold_1/concat_fold_1.dev.predictions.0.step.${step}.csv gs://acl-2022-storage/concat_fold_1/
-	rm -r -f $HOME/concat_fold_1/model_0_step_${step}_model
+		--prediction_file $HOME/concat_fold_1/concat_fold_1.dev.predictions.1.step.${step}.csv
+	gsutil -m cp $HOME/concat_fold_1/concat_fold_1.dev.predictions.1.step.${step}.csv gs://acl-2022-storage/concat_fold_1/
+	rm -r -f $HOME/concat_fold_1/model_1_step_${step}_model
 done
 
-gsutil -m cp gs://acl-2022-storage/concat_fold_1/model_0_model $HOME/concat_fold_1/
+gsutil -m cp gs://acl-2022-storage/concat_fold_1/model_1_model $HOME/concat_fold_1/
 printf "Full epoch 1 \r\n"
 python src/re_gold_qa_train.py \
 	--mode re_concat_qa_test \
 	--model_path $HOME/concat_fold_1/ \
-	--checkpoint _0_model \
+	--checkpoint _1_model \
 	--learning_rate 0.001 --max_epochs 1 \
 	--concat_questions False \
 	--batch_size 64  --gpu True \
@@ -39,9 +39,9 @@ python src/re_gold_qa_train.py \
 	--dev zero-shot-extraction/relation_splits/dev.0 \
 	--gpu_device 0 \
 	--seed 12321 \
-	--prediction_file $HOME/concat_fold_1/concat_fold_1.dev.predictions.0.full.csv
-gsutil -m cp $HOME/concat_fold_1/concat_fold_1.dev.predictions.0.full.csv gs://acl-2022-storage/concat_fold_1/
-rm -r -f $HOME/concat_fold_1/model_0_model
+	--prediction_file $HOME/concat_fold_1/concat_fold_1.dev.predictions.1.full.csv
+gsutil -m cp $HOME/concat_fold_1/concat_fold_1.dev.predictions.1.full.csv gs://acl-2022-storage/concat_fold_1/
+rm -r -f $HOME/concat_fold_1/model_1_model
 
 gsutil -m cp gs://acl-2022-storage/concat_fold_1/model_response_pretrained_model $HOME/concat_fold_1/
 printf "step 0\r\n"
@@ -57,6 +57,6 @@ python src/re_gold_qa_train.py \
 	--dev zero-shot-extraction/relation_splits/dev.0 \
 	--gpu_device 0 \
 	--seed 12321 \
-	--prediction_file $HOME/concat_fold_1/concat_fold_1.dev.predictions.0.step.0.csv
-gsutil -m cp $HOME/concat_fold_1/concat_fold_1.dev.predictions.0.step.0.csv gs://acl-2022-storage/concat_fold_1/
+	--prediction_file $HOME/concat_fold_1/concat_fold_1.dev.predictions.1.step.0.csv
+gsutil -m cp $HOME/concat_fold_1/concat_fold_1.dev.predictions.1.step.0.csv gs://acl-2022-storage/concat_fold_1/
 rm -r -f $HOME/concat_fold_1/model_response_pretrained_model
--- test_re_gold_qa.sh
+++ test_re_gold_qa.sh
@@ -2,16 +2,16 @@
 
 source env/bin/activate
 
-printf "fold 1, epoch 1\r\n"
+printf "fold 1, epoch 2\r\n"
 for (( i=1; i<=131; i++ ))
 do
 	step=$((i * 100))
 	printf "step ${step}\r\n"
-	gsutil -m cp gs://acl-2022-storage/gold_fold_1/model_0_step_${step}_model $HOME/gold_fold_1/
+	gsutil -m cp gs://acl-2022-storage/gold_fold_1/model_1_step_${step}_model $HOME/gold_fold_1/
 	python src/re_gold_qa_train.py \
 		--mode re_gold_qa_test \
 		--model_path $HOME/gold_fold_1/ \
-		--checkpoint _0_step_${step}_model \
+		--checkpoint _1_step_${step}_model \
 		--learning_rate 0.001 --max_epochs 1 \
 		--concat_questions False \
 		--batch_size 64  --gpu True \
@@ -20,17 +20,17 @@ do
 		--dev zero-shot-extraction/relation_splits/dev.0 \
 		--gpu_device 0 \
 		--seed 12321 \
-		--prediction_file $HOME/gold_fold_1/gold_fold_1.dev.predictions.0.step.${step}.csv
-	gsutil -m cp $HOME/gold_fold_1/gold_fold_1.dev.predictions.0.step.${step}.csv gs://acl-2022-storage/gold_fold_1/
-	rm -r -f $HOME/gold_fold_1/model_0_step_${step}_model
+		--prediction_file $HOME/gold_fold_1/gold_fold_1.dev.predictions.1.step.${step}.csv
+	gsutil -m cp $HOME/gold_fold_1/gold_fold_1.dev.predictions.1.step.${step}.csv gs://acl-2022-storage/gold_fold_1/
+	rm -r -f $HOME/gold_fold_1/model_1_step_${step}_model
 done
 
-gsutil -m cp gs://acl-2022-storage/gold_fold_1/model_0_model $HOME/gold_fold_1/
+gsutil -m cp gs://acl-2022-storage/gold_fold_1/model_1_model $HOME/gold_fold_1/
 printf "Full epoch 1 \r\n"
 python src/re_gold_qa_train.py \
 	--mode re_gold_qa_test \
 	--model_path $HOME/gold_fold_1/ \
-	--checkpoint _0_model \
+	--checkpoint _1_model \
 	--learning_rate 0.001 --max_epochs 1 \
 	--concat_questions False \
 	--batch_size 64  --gpu True \
@@ -39,9 +39,9 @@ python src/re_gold_qa_train.py \
 	--dev zero-shot-extraction/relation_splits/dev.0 \
 	--gpu_device 0 \
 	--seed 12321 \
-	--prediction_file $HOME/gold_fold_1/gold_fold_1.dev.predictions.0.full.csv
-gsutil -m cp $HOME/gold_fold_1/gold_fold_1.dev.predictions.0.full.csv gs://acl-2022-storage/gold_fold_1/
-rm -r -f $HOME/gold_fold_1/model_0_model
+	--prediction_file $HOME/gold_fold_1/gold_fold_1.dev.predictions.1.full.csv
+gsutil -m cp $HOME/gold_fold_1/gold_fold_1.dev.predictions.1.full.csv gs://acl-2022-storage/gold_fold_1/
+rm -r -f $HOME/gold_fold_1/model_1_model
 
 gsutil -m cp gs://acl-2022-storage/gold_fold_1/model_response_pretrained_model $HOME/gold_fold_1/
 printf "step 0\r\n"
@@ -57,6 +57,6 @@ python src/re_gold_qa_train.py \
 	--dev zero-shot-extraction/relation_splits/dev.0 \
 	--gpu_device 0 \
 	--seed 12321 \
-	--prediction_file $HOME/gold_fold_1/gold_fold_1.dev.predictions.0.step.0.csv
-gsutil -m cp $HOME/gold_fold_1/gold_fold_1.dev.predictions.0.step.0.csv gs://acl-2022-storage/gold_fold_1/
+	--prediction_file $HOME/gold_fold_1/gold_fold_1.dev.predictions.1.step.0.csv
+gsutil -m cp $HOME/gold_fold_1/gold_fold_1.dev.predictions.1.step.0.csv gs://acl-2022-storage/gold_fold_1/
 rm -r -f $HOME/gold_fold_1/model_response_pretrained_model
