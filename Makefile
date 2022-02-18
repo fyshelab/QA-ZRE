555b77d04a97f4af05a47afbbb81721c6a5d4b79
Saeed Najafi <snajafi@ualberta.ca>
2022-02-18 17:45:51 -0500

    Test the code on lambda server

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: install
+.PHONY: install_narval
 install:
 
 	python3 -m venv env; \
@@ -6,6 +6,8 @@ install:
 	pip3 install -e .; \
 	pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -U -f https://download.pytorch.org/whl/torch_stable.html --no-cache-dir; \
 	pip3 install nltk rouge-score sentencepiece absl-py; \
+	module load StdEnv/2020 gcc/9.3.0 cuda/11.4 arrow/5.0.0; \
+	pip3 install pyarrow==5.0.0; \
 	pip3 install datasets; \
 	pip3 install spacy; \
 
@@ -25,3 +27,13 @@ install_gsutil:
 	bash google-cloud-sdk/install.sh
 	google-cloud-sdk/bin/gcloud init
 
+.PHONY: install
+install:
+
+	python3 -m venv env; \
+	. env/bin/activate; \
+	pip3 install -e .; \
+	pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -U -f https://download.pytorch.org/whl/torch_stable.html --no-cache-dir; \
+	pip3 install nltk rouge-score sentencepiece absl-py; \
+	pip3 install datasets; \
+	pip3 install spacy; \
--- mml_pgg_off_sim.sh
+++ mml_pgg_off_sim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=train_mml_pgg_off_sim_fold_1
+#SBATCH --job-name=train_mml_mml_off_sim_fold_2
 #SBATCH --account=def-afyshe-ab
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=24000M
-#SBATCH --time=5-00:00
+#SBATCH --time=1-00:00
 #SBATCH --cpus-per-task=3
 #SBATCH --output=%N-%j.out
 
@@ -22,17 +22,17 @@ echo "All the allocated nodes: $SLURM_JOB_NODELIST"
 
 srun python src/re_gold_qa_train.py \
     --mode re_qa_train \
-    --model_path $SCRATCH/feb-15-arr/fold_1/mml-pgg-off-sim/ \
+    --model_path $SCRATCH/feb-15-arr/fold_2/mml-mml-off-sim/ \
     --answer_checkpoint _response_pretrained \
     --question_checkpoint _fold_1_question_pretrained \
-    --training_steps 52400 \
+    --training_steps 10000 \
     --learning_rate 0.0005 \
     --max_epochs 1 \
     --num_search_samples 8 \
     --batch_size 16 \
     --gpu True \
-    --dev ./zero-shot-extraction/relation_splits/dev.0 \
-    --train ./zero-shot-extraction/relation_splits/train.0 \
+    --dev ./zero-shot-extraction/relation_splits/dev.1 \
+    --train ./zero-shot-extraction/relation_splits/train.1 \
     --gpu_device 0 \
     --seed 12321 \
-    --train_method MML-PGG-Off-Sim
+    --train_method MML-MML-Off-Sim
